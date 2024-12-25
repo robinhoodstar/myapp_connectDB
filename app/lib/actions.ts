6afef0b684d7ec8b04356221a064477b72c2b828
robinhoodstar <robinhoodstar80@gmail.com>
2024-12-25 01:21:21 -0600

    Next_1_14

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -64,7 +64,7 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-  // throw new Error('Failed to Delete Invoice');
+  throw new Error('Failed to Delete Invoice');
 
   // Unreachable code block
   try {
